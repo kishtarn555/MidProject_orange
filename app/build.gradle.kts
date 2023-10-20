186f56a3a5e9cdc8b1ce2a76b6ef83ab2c8783b4
artmtt <aaaajjjj72@gmail.com>
2023-10-19 19:44:35 -0600

    Push test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.midproject_orange"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.midproject_orange"
