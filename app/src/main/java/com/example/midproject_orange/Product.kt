0c170d80368300757583dc9c0557d6e46a4fa16d
Hector Fernando Ricardez Lara <hec_montemoon@hotmail.com>
2023-11-01 18:30:30 -0600

    SQL First commit

---
+++ app/src/main/java/com/example/midproject_orange/Product.kt
@@ -0,0 +1,3 @@
+package com.example.midproject_orange
+
+data class Product(val id: Int, val name: String, val price: Float, val quantity: Int)
---
+++ app/src/main/java/com/example/midproject_orange/SqlLiteHelper.kt
@@ -0,0 +1,59 @@
+package com.example.sqliteproject
+
+import android.content.ContentValues
+import  android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import  android.database.sqlite.SQLiteOpenHelper
+import com.example.midproject_orange.Product
+
+class SQLiteHelper(context:Context) :  SQLiteOpenHelper(context, "agenda_info", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        val script = "CREATE TABLE products"+
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT,"+
+                "price DECIMAL,"+
+                "quantity INT)"
+        db!!.execSQL(script)
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        val script = "DROP TABLE IF EXISTS products"
+        db!!.execSQL(script)
+        onCreate(db)
+    }
+
+
+    fun insert(name:String, price:Float, quanity:Int) {
+        val data = ContentValues()
+        data.put("name", name)
+        data.put("price", price)
+        data.put("quantity", quanity)
+        val db = this.writableDatabase
+        db.insert("products",null, data)
+        db.close()
+    }
+
+    fun getAllProducts(): List<Product> {
+        val productList = mutableListOf<Product>()
+        val query = "SELECT * FROM products"
+        val db = this.readableDatabase
+        val cursor = db.rawQuery(query, null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+                val name = cursor.getString(cursor.getColumnIndexOrThrow("name"))
+                val price = cursor.getFloat(cursor.getColumnIndexOrThrow("price"))
+                val quantity = cursor.getInt(cursor.getColumnIndexOrThrow("quantity"))
+                val product = Product(id, name, price, quantity)
+                productList.add(product)
+            } while (cursor.moveToNext())
+        }
+
+        cursor.close()
+        db.close()
+        return productList
+    }
+
+}
